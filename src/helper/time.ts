1ab9261ac7d99f1704144953ee4e3dbed9d14a7f
Eric020615 <ericjuncheng10@gmail.com>
2024-08-12 08:27:20 +0800

    change timezone to UTC

--- src/helper/time.ts
+++ src/helper/time.ts
@@ -2,13 +2,11 @@ import { Timestamp } from "firebase/firestore";
 import moment from "moment";
 import "moment-timezone";
 
-const serverTimezone = "Asia/Kuala_Lumpur"
-
 export const convertDateStringToTimestamp = (dateString: string) => {
   if(!dateString){
     return null
   }
-  return Timestamp.fromDate(moment(dateString).toDate())
+  return Timestamp.fromDate(moment(dateString).utc().toDate())
 }
 
 
@@ -16,9 +14,9 @@ export const convertTimestampToUserTimezone = (timestamp: Timestamp | null) => {
   if(!timestamp){
     return ""
   }
-  return moment(timestamp.toDate()).tz(serverTimezone).format('YYYY-MM-DD HH:mm:ss');
+  return moment(timestamp.toDate()).format('YYYY-MM-DD HH:mm:ss');
 }
 
 export const getNowTimestamp = () => {
-  return Timestamp.fromDate(moment().tz(serverTimezone).toDate());
+  return Timestamp.fromDate(moment().utc().toDate());
 }
